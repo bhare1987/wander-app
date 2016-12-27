e66c202bb7216b481eef80e758e15fc82bd5c08f
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2016-12-27 11:31:46 -0500

    Hiding back button, adding sideDrawer.

--- app/components/friends/friends.component.js
+++ app/components/friends/friends.component.js
@@ -1,14 +1,25 @@
 "use strict";
 var core_1 = require("@angular/core");
+var frame_1 = require("ui/frame");
+var sidedrawer_service_1 = require("../../shared/sidedrawer.service");
 var FriendsComponent = (function () {
-    function FriendsComponent() {
+    function FriendsComponent(sideDrawerService) {
+        this.sideDrawerService = sideDrawerService;
     }
+    FriendsComponent.prototype.ngOnInit = function () {
+        this.controller = frame_1.topmost().ios.controller;
+        this.navItem = this.controller.visibleViewController.navigationItem;
+        this.navItem.setHidesBackButtonAnimated(true, false);
+    };
+    FriendsComponent.prototype.toggleMenu = function () {
+        this.sideDrawerService.toggleMenu();
+    };
     FriendsComponent = __decorate([
         core_1.Component({
             selector: "wander-friends",
             templateUrl: "./components/friends/friends.component.html",
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [sidedrawer_service_1.SideDrawerService])
     ], FriendsComponent);
     return FriendsComponent;
 }());
--- app/components/friends/friends.component.ts
+++ app/components/friends/friends.component.ts
@@ -1,9 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { topmost } from "ui/frame";
+import { isIOS } from "platform";
+import { SideDrawerService } from "../../shared/sidedrawer.service";
 
 @Component({
     selector: "wander-friends",
     templateUrl: "./components/friends/friends.component.html",
 })
-export class FriendsComponent {
+export class FriendsComponent implements OnInit {
+    public controller;
+    public navItem;
+    constructor( private sideDrawerService: SideDrawerService ) {
+
+    }
+    
+    ngOnInit() {
+        this.controller = topmost().ios.controller;
+        this.navItem = this.controller.visibleViewController.navigationItem;
+        this.navItem.setHidesBackButtonAnimated(true, false);
+    }
+
+    toggleMenu() {
+        this.sideDrawerService.toggleMenu();
+    }
+    
+
+
 
 }
